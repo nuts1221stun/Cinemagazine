f304f3d4de8b0119499e59ac80528c5fa1f813da
Li-Erh Chang <lechang@yahoo-inc.com>
2018-09-13 22:15:51 +0800

    Listing: add listing view controller

--- Cinemagazine/AppDelegate.swift
+++ Cinemagazine/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let vc = CNMListingViewController()
+        let navVC = UINavigationController(rootViewController: vc)
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = navVC
+        window.makeKeyAndVisible()
+        self.window = window
+
         return true
     }
 
---
+++ Cinemagazine/Listing/Controllers/CNMListingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CNMListingViewController.swift
+//  Cinemagazine
+//
+//  Created by Li-Erh Chang on 2018/9/13.
+//  Copyright © 2018 nuts. All rights reserved.
+//
+
+import UIKit
+
+class CNMListingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+}
